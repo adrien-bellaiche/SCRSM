3eb6ce37e20927123227f670992a3250c6d0f8a7
adrien-bellaiche <adrien.bellaiche@gmail.com>
2014-03-25 11:35:10 +0100

    test

--- Test.py
+++ Test.py
@@ -4,8 +4,6 @@ from ServerTricked import *
 from MoteurPhysique import *
 from Physique import *
 
-print("test")
-
 server = ModbusServer()
 #server.setValue(2, 255 * 255)
 server.set_prop_front_left(100) # avant gauche
