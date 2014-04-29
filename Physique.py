b41e7109c553cb93a390ee0f33fc611c93cdb345
Adrien <adrien.bellaiche@gmail.com>
2014-04-29 10:31:34 +0200

    version commune

--- Physique.py
+++ Physique.py
@@ -197,7 +197,7 @@ class ObjetPhysique():
         t = vect(self, target) # vector self->Target
         us = [rotation([1, 0, 0], self.mat), rotation([0, 1, 0], self.mat), rotation([0, 0, 1], self.mat),
               rotation([1, 0, 0], target.mat), rotation([0, 1, 0], target.mat), rotation([0, 0, 1], target.mat)]
-        # Note : voir si la version basique est encore justifiée (chrono ?)
+        # Todo : voir si la version basique est encore justifiée (chrono ?)
         for k in range(6):
             L = us[k]
             left = abs(dot(t, L))  # si >right, alors c'est un axe separateur.
@@ -288,9 +288,15 @@ class Robot(ObjetPhysique):
 
 
 class Sphere(ObjetPhysique):  # Fini, a tester
-    def __init__(self, x, y, z, r):
+    def __init__(self, *args):
+        if len(args) == 1:
+            x, y, z, r = args[0]
+        elif len(args) == 4:
+            x, y, z, r = args[0:4]
         super().__init__(x, y, z, 0, 0, 0, 2 * r, 2 * r, 2 * r)
         self.rayon = r
+
+
 
     def accurate_collision(self, robot):
         # Distance a chaque plan genere par les faces "repositionne" au centre du robot. Si au moins une est superieure a self.rayon + base[i]/2 alors, pas de collision. Sinon, collision.
@@ -303,7 +309,11 @@ class Sphere(ObjetPhysique):  # Fini, a tester
 
 
 class Cylindre(ObjetPhysique):  # si les angles sont a 0, c'est un cylindre d'axe Z
-    def __init__(self, x, y, z, r, h, theta, phi, psi):
+    def __init__(self, *args):
+        if len(args) == 8:
+            x, y, z, r, h, theta, phi, psi = args[0:8]
+        elif len(args) == 1:
+            x, y, z, r, h, theta, phi, psi = args[0]
         super().__init__(x, y, z, theta, phi, psi, 2 * r, 2 * r, h)
         self.rayon = r
         self.hauteur = h
@@ -313,4 +323,5 @@ class Pave(ObjetPhysique):
     def __init__(self, x, y, z, theta, phi, psi, lo, la, he):
         #longueur selon x, largeur selon y, hauteur selon z (de base)
         super().__init__(x, y, z, theta, phi, psi, lo, la, he)
+
 
--- test2.py
+++ test2.py
@@ -1,10 +1,12 @@
-from MoteurPhysique import MoteurPhysique
-from Physique import *
-from ServerTricked import *
+
 __author__ = 'Adrien'
 
-r = Robot(0)
-server = ModbusServer()
-mot = MoteurPhysique(r,server, 0.001, 50, 9.81, 1)
-mot.obstacles.append(Pave(2, 2, 1.5, 0, -pi/12, pi/4, 5, 5, 5))
-print(mot.detect_collisions())
+
+def f(*args):
+    for arg in args:
+        print(arg)
+
+
+p=[1,2,3,4,5,6,7]
+print(p[0:3])
+f(1,2,3)
