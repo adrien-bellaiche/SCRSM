3614f132eaf34fa51c71be88070c799eaf3fdf33
adrien-bellaiche <adrien.bellaiche@gmail.com>
2014-05-13 13:14:37 +0200

    Update Commandes.py

--- Commandes.py
+++ Commandes.py
@@ -2,8 +2,9 @@ from time import sleep
 
 def en_bas(server,puissance=50,duree=3):
     server.set_prop_vertical(puissance)
-    sleep(duree)
-    stop(server)
+    if duree > 0 :
+        sleep(duree)
+        stop(server)
     
 def en_haut(server,puissance=50,duree=3):
     en_bas(server,-puissance,duree)
@@ -14,32 +15,35 @@ def en_avant(server,puissance=50,duree=3):
     server.set_prop_rear_left(-puissance)
     server.set_prop_rear_right(-puissance)
     server.set_prop_vertical(0)
-    sleep(duree)
-    stop(server)
+    if duree > 0 :
+        sleep(duree)
+        stop(server)
 
 def en_arriere(server,puissance=50,duree=3):
     en_avant(server,-puissance,duree)
 
-def a_droite(server,puissance=50,duree=3):
+def crabe_droite(server,puissance=50,duree=3):
     server.set_prop_front_left(puissance)
     server.set_prop_front_right(-puissance)
     server.set_prop_rear_left(-puissance)
     server.set_prop_rear_right(puissance)
     server.set_prop_vertical(0)
-    sleep(duree)
-    stop(server)
+    if duree > 0 :
+        sleep(duree)
+        stop(server)
 
 def a_gauche(server,puissance=50,duree=3):
     a_droite(server,-puissance,duree)
 
-def crabe_droite(server,puissance=50,duree=3):
+def a_droite(server,puissance=50,duree=3):
     server.set_prop_front_left(puissance)
     server.set_prop_front_right(-puissance)
     server.set_prop_rear_left(puissance)
     server.set_prop_rear_right(-puissance)
     server.set_prop_vertical(0)
-    sleep(duree)
-    stop(server)
+    if duree > 0 :
+        sleep(duree)
+        stop(server)
 
 def crabe_gauche(server,puissance=50,duree=3):
     crabe_droite(server,-puissance,duree)
